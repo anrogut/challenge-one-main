1b3d5fd9c9c6c7937913215096db5d969d309409
Adrian <adrian.rogut@gmail.com>
2016-12-14 12:16:20 +0100

    Fix formatting in js file

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -48,6 +48,6 @@ $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
+    $("#connect").click(function() { connect(); });
+    $("#disconnect").click(function() { disconnect(); });
 });

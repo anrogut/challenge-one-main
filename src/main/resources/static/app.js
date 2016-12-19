56b80335b9d92b8877e4240fe2e6aa46515c01f9
Adrian <adrian.rogut@gmail.com>
2016-12-19 13:11:24 +0100

    Switch double quotes to single quote

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -45,7 +45,7 @@ function addEvent(event) {
 function addFileToStructure(file) {
     var fileObj = JSON.parse(file);
     var humanReadablePath = decodeURIComponent(fileObj.path.substr(8)); // remove 'file:///'
-    var iconClass = humanReadablePath.charAt(humanReadablePath.length - 1) === '/' ? "glyphicon glyphicon-folder-open" : "glyphicon glyphicon-file";
+    var iconClass = humanReadablePath.charAt(humanReadablePath.length - 1) === '/' ? 'glyphicon glyphicon-folder-open' : 'glyphicon glyphicon-file';
     $('#files').append('<tr><td><span class="' + iconClass + '"></span>    ' + humanReadablePath + '</td></tr>')
 }
 
